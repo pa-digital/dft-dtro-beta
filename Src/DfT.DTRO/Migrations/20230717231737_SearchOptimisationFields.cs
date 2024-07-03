e98f534c520d2817e03d61cc59114c79eeae746b
Ross Hockly <Ross.Hockly@PACONSULTING.COM>
2024-07-03 16:57:44 +0100

    added migrations

---
+++ Src/DfT.DTRO/Migrations/20230717231737_SearchOptimisationFields.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DfT.DTRO.Migrations;
+public partial class SearchOptimisationFields : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "HA",
+            table: "Dtros",
+            type: "integer",
+            nullable: false,
+            defaultValue: 0);
+
+        migrationBuilder.AddColumn<string>(
+            name: "TroName",
+            table: "Dtros",
+            type: "text",
+            nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "HA",
+            table: "Dtros");
+
+        migrationBuilder.DropColumn(
+            name: "TroName",
+            table: "Dtros");
+    }
+}
