a93ee517565d0d7c3bbdf112557a70b419dfef00
Ross Hockly <Ross.Hockly@PACONSULTING.COM>
2024-07-05 13:56:33 +0100

    Working pie

--- Src/Dft.DTRO.Admin/Pages/Index.cshtml.cs
+++ Src/Dft.DTRO.Admin/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,8 +12,62 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public MetricSummary Metrics { get; set; }
+    public bool HealthApi { get; set; } = true;
+
+    public bool TraIdMatch { get; set; } = false;
+
+    public bool HealthDatabase { get; set; } = true;
+
     public void OnGet()
     {
-
+        Metrics = new MetricSummary
+        {
+            SystemFailureCount = 0,
+            SubmissionFailureCount = 0,
+            SubmissionCount = 0,
+            DeletionCount = 0,
+            SearchCount = 0,
+            EventCount = 0
+        };
     }
+}
+
+public class MetricSummary
+{
+    /// <summary>
+    /// Gets or sets system Failure Count Metric.
+    /// </summary>
+    [DataMember(Name = "systemFailureCount")]
+    public int SystemFailureCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets Submission Failure Count Metric.
+    /// </summary>
+    [DataMember(Name = "submissionFailureCount")]
+    public int SubmissionFailureCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets Submission Count Metric.
+    /// </summary>
+    [DataMember(Name = "submissionCount")]
+    public int SubmissionCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets Deletion Count Metric.
+    /// </summary>
+    [DataMember(Name = "deletionCount")]
+    public int DeletionCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets Search Count Metic.
+    /// </summary>
+    [DataMember(Name = "searchCount")]
+    public int SearchCount { get; set; }
+
+    /// <summary>
+    /// Gets or sets Event Count Metic.
+    /// </summary>
+    [DataMember(Name = "eventCount ")]
+    public int EventCount { get; set; }
 }
