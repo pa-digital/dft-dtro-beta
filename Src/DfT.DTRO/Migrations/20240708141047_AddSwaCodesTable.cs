126de4b9b3464bbb587d9e8d852a3a878a0ca375
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-08 15:13:31 +0100

    SWA Codes tables inserted

---
+++ Src/DfT.DTRO/Migrations/20240708141047_AddSwaCodesTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DfT.DTRO.Migrations
+{
+    public partial class AddSwaCodesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SwaCodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    Prefix = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SwaCodes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SwaCodes");
+        }
+    }
+}
---
+++ Src/DfT.DTRO/Models/DataBase/SwaCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace DfT.DTRO.Models.DataBase;
+
+[DataContract]
+public class SwaCode
+{
+    /// <summary>
+    /// Gets or sets id of the SWA.
+    /// </summary>
+    [Key]
+    [SwaggerSchema(ReadOnly = true)]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Column(TypeName = "uuid")]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the SWA code of the request.
+    /// </summary>
+    /// <example>1585</example>
+    [Required(ErrorMessage = "SWA code field must be included")]
+    [DataMember(Name = "code")]
+    public int Code { get; set; }
+
+    /// <summary>
+    /// Gets or sets the SWA name of the request.
+    /// </summary>
+    [Required(ErrorMessage = "SWA name field must be included")]
+    [DataMember(Name = "name")]
+    [StringLength(int.MaxValue)]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the SWA prefix of the request.
+    /// </summary>
+    [Required(ErrorMessage = "SWA prefix field must be included")]
+    [DataMember(Name = "prefix")]
+    [StringLength(20)]
+    public string Prefix { get; set; }
+}
