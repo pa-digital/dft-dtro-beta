840c05198789876b7f05833a20a17c046252a914
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-18 12:01:38 +0100

    adjust files for new deployment

--- Src/DfT.DTRO/postgres.sh
+++ Src/DfT.DTRO/postgres.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-apk --update add postgresql-client
-chmod +x ./D-TRO_Database.sql
-psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f ./D-TRO_Database.sql
-
-exec ./Dft.DTRO
+apk update && apk add postgresql-client
+chmod +x D-TRO_Database.sql
+PGPASSWORD=11jul
+psql -h 127.0.0.1 -U 11jul  -d dtro-dev-database -f D-TRO_Database.sql
+exec Dft.DTRO
