9ad37118dce8698416a9e0c771f48435869b87fa
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-18 11:40:12 +0100

    modify the bash

--- Src/DfT.DTRO/postgres.sh
+++ Src/DfT.DTRO/postgres.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#Add PostgreSQL 'D-TRO_Database.sql' script
-apk add postgresql-client
-chmod +x /app/D-TRO_Database.sql
-psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f /app/D-TRO_Database.sql
+apk --update add postgresql-client
+chmod +x ./D-TRO_Database.sql
+psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f ./D-TRO_Database.sql
 
 exec ./Dft.DTRO
