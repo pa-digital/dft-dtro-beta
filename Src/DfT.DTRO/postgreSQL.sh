779d292f1a91b0daf0d30ef3f9372cd535eb4342
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-18 14:49:38 +0100

    hard-coded the path for running the bash script

--- Src/DfT.DTRO/postgreSQL.sh
+++ Src/DfT.DTRO/postgreSQL.sh
@@ -1,3 +1,6 @@
-﻿#!/bin/sh
+﻿export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+ls
+psql --help
+which psql
 psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f D-TRO_Database.sql
-exec /DfT.DTRO
+exec DfT.DTRO
