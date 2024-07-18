b8c6c6798c2e0c095309024a967fe8a656b573fc
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-18 14:59:24 +0100

    fix the docker and postreSQL files

--- Src/DfT.DTRO/postgreSQL.sh
+++ Src/DfT.DTRO/postgreSQL.sh
@@ -1,6 +1,2 @@
-﻿export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-ls
-psql --help
-which psql
-psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f D-TRO_Database.sql
+﻿psql -h 127.0.0.1 -U 11jul -d dtro-dev-database -f D-TRO_Database.sql
 exec DfT.DTRO
