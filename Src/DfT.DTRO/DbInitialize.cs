037eb169f156d11d7b16d788c41b76be1c263801
Gabriel-Popescu <151055246+Gabriel-Popescu@users.noreply.github.com>
2024-07-29 12:26:40 +0100

    Add SQL statement to test the existing of SwaCode table.

---
+++ Src/DfT.DTRO/DbInitialize.cs
@@ -0,0 +1,16 @@
+﻿using DfT.DTRO.DAL;
+
+namespace DfT.DTRO;
+
+public static class DbInitialize
+{
+    public static void RunSqlStatement(DtroContext dtroContext)
+    {
+        Console.WriteLine(DateTime.UtcNow);
+        int swaCodesCount = dtroContext.SwaCodes.Count();
+        Console.WriteLine(swaCodesCount);
+        List<string> swaCodesNames = dtroContext.SwaCodes.Select(swaCode => swaCode.Name).ToList();
+        swaCodesNames.ForEach(Console.WriteLine);
+        Console.WriteLine(DateTime.UtcNow);
+    }
+}
