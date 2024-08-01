796b2735042288219d6adf5c06838e65ff725bdb
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-01 11:51:07 +0100

    DPPB-488: initial commit
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- terraform/container_runtime.tf
+++ terraform/container_runtime.tf
@@ -4,7 +4,7 @@ locals {
   max_instance_count   = floor(var.database_max_connections / var.db_connections_per_cloud_run_instance)
   db_password_env_name = "POSTGRES_PASSWORD"
 
-  common_service_envs = merge(
+  dtro_service_envs = merge(
     {
       DEPLOYED               = timestamp()
       PROJECTID              = data.google_project.project.project_id
@@ -46,7 +46,7 @@ resource "google_cloud_run_v2_service" "dtro_service" {
     }
 
     containers {
-      image = "${var.artifact_registry_image_path}:${var.tag}"
+      image = "${var.artifact_registry_dtro_image_path}:${var.tag}"
 #       # TODO: Below is the last stable image
 #       image = "europe-west1-docker.pkg.dev/dft-dtro-dev-01/dft-dtro-dev-repository/dft-dtro-beta@sha256:f34febca186167410eb8ee2a8362975521c8994c675ba22a5590cb563d442e0f"
       ports {
@@ -54,7 +54,7 @@ resource "google_cloud_run_v2_service" "dtro_service" {
       }
 
       dynamic "env" {
-        for_each = local.common_service_envs
+        for_each = local.dtro_service_envs
         content {
           name  = env.key
           value = env.value
@@ -93,6 +93,68 @@ resource "google_cloud_run_v2_service" "dtro_service" {
       name  = "cloud-sql-proxy"
       image = "gcr.io/cloud-sql-connectors/cloud-sql-proxy:latest"
       args  = ["--private-ip", data.google_sql_database_instance.postgres_db.connection_name]
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service" "service_portal_service" {
+  name     = "${local.cloud_run_service_name}-service-UI"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    service_account = var.execution_service_account
+
+    scaling {
+      min_instance_count = 1
+      max_instance_count = 1
+    }
+
+    vpc_access {
+      connector = data.google_vpc_access_connector.serverless_connector.id
+      egress    = "PRIVATE_RANGES_ONLY"
+    }
+
+    containers {
+      image = "${var.artifact_registry_service_ui_image_path}:${var.tag}"
+      ports {
+        container_port = 8080
+      }
+
+      dynamic "env" {
+        for_each = local.dtro_service_envs
+        content {
+          name  = env.key
+          value = env.value
+        }
+      }
+
+      env {
+        name = local.db_password_env_name
+        value_source {
+          secret_key_ref {
+            secret  = data.google_secret_manager_secret_version.postgres_password_value.secret
+            version = "latest"
+          }
+        }
+      }
+
+      startup_probe {
+        timeout_seconds   = 3
+        period_seconds    = 15
+        failure_threshold = 10
+        http_get {
+          path = "/health"
+          port = 8080
+        }
+      }
+
+      liveness_probe {
+        http_get {
+          path = "/health"
+          port = 8080
+        }
+      }
     }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,13 +33,25 @@ variable "execution_service_account" {
   description = "Service account for executing GCP applications."
 }
 
-variable "artifact_registry_image_path" {
+variable "artifact_registry_dtro_image_path" {
   type        = string
   description = "Path of the image in Artifact Registry"
   default     = "europe-west1-docker.pkg.dev/dft-dtro-dev-01/dft-dtro-dev-repository/dft-dtro-beta"
 }
 
+variable "artifact_registry_service_ui_image_path" {
+  type        = string
+  description = "Path of the image in Artifact Registry"
+  default     = "europe-west1-docker.pkg.dev/dft-dtro-dev-01/dft-dtro-dev-ui-repository/dft-dtro-beta"
+}
+
 variable "dtro_service_image" {
+  type        = string
+  description = "The name of an image being pushed for publish service."
+  default     = "dft-dtro-beta"
+}
+
+variable "service_ui_image" {
   type        = string
   description = "The name of an image being pushed for publish service."
   default     = "dft-dtro-beta"
