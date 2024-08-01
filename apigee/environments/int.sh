92a5f1fb608ad482b114b46d77e11fb7b33427d2
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-01 08:47:30 +0100

    DPPB-420: initial commit
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/environments/int.sh
+++ apigee/environments/int.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "env=prod" >> $GITHUB_ENV
+echo "env=test" >> $GITHUB_ENV
+echo "env_name_prefix=int" >> $GITHUB_ENV
 echo "workload_identity_service_account=${service_account}" >> $GITHUB_ENV
 echo "apigee_organisation=${apigee_organisation}" >> $GITHUB_ENV
-echo "target_url=http://7.0.8.4" >> $GITHUB_ENV
+echo "target_url=http://7.0.8.5" >> $GITHUB_ENV
