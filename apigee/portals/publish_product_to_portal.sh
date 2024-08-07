c137bca5bc69ce4f8f9f931f70f74ed2f8f04097
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-07 16:00:39 +0100

    DPPB-355: Update files for creating OAuth polices and api product - #5
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/portals/publish_product_to_portal.sh
+++ apigee/portals/publish_product_to_portal.sh
@@ -7,13 +7,13 @@ to_title_case() {
   echo "$1" | sed -e 's/\b./\u&/g' -e 's/-/ /g'
 }
 TITLE_ENV=$(to_title_case "${env}")
-PORTAL_NAME="${TITLE_ENV} Developer NPP Portal"
+PORTAL_NAME="${TITLE_ENV} Developer D-TRO Portal"
 PORTAL_URL="$(echo "${PORTAL_NAME//[- ]/}" | tr '[:upper:]' '[:lower:]')"
 
 TOKEN=$1
 
 # List of product names
-PRODUCT_NAMES=("service-provider" "enforcement-provider" "parking-operator")
+PRODUCT_NAMES=("cso" "digital-service-provider" "data-consumer")
 
 # Loop through each product name
 for PRODUCT in "${PRODUCT_NAMES[@]}"; do
