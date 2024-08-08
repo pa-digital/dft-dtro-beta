a7131effc02bdc53861d30b921349260699f9a09
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-08 21:41:31 +0100

    DPPB-355: Formating updated for Product names
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/portals/publish_product_to_portal.sh
+++ apigee/portals/publish_product_to_portal.sh
@@ -21,10 +21,10 @@ for PRODUCT in "${PRODUCT_NAMES[@]}"; do
   PRODUCT_NAME="${env}-${PRODUCT}"
 
 #  # Convert product name to title case with spaces
-#  TITLE=$(to_title_case "${PRODUCT_NAME}")
+  TITLE=$(to_title_case "${PRODUCT}")
 
   # Construct the description
-  DESCRIPTION="This is the ${TITLE_ENV} D-TRO application for ${PRODUCT}."
+  DESCRIPTION="This is the ${TITLE_ENV} D-TRO application for ${PRODUCT}s."
 
   # Make the API call
   RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites/${ORG}-${PORTAL_URL}/apidocs" \
