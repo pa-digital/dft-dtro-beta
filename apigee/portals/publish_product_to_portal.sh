e984c2b6cbc61462c8683a3ffe6cb4c31b2be13d
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-08 12:58:11 +0100

    DPPB-355: Update files for creating OAuth polices and api product - #10
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/portals/publish_product_to_portal.sh
+++ apigee/portals/publish_product_to_portal.sh
@@ -20,11 +20,11 @@ for PRODUCT in "${PRODUCT_NAMES[@]}"; do
 
   PRODUCT_NAME="${env}-${PRODUCT}"
 
-  # Construct the description
-  DESCRIPTION="This is the ${PRODUCT_NAME} product."
-
   # Convert product name to title case with spaces
   TITLE=$(to_title_case "${PRODUCT_NAME}")
+
+  # Construct the description
+  DESCRIPTION="This is the D-TRO application for ${TITLE}."
 
   # Make the API call
   RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites/${ORG}-${PORTAL_URL}/apidocs" \
