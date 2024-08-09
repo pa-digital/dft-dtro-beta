b0dcf6236efa4bb43473acaca1e33808eca4c35e
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-08-09 21:05:20 +0100

    DPPB-355: Update target endpoint
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/environments/int.sh
+++ apigee/environments/int.sh
@@ -4,4 +4,4 @@ echo "env=test" >> $GITHUB_ENV
 echo "env_name_prefix=int" >> $GITHUB_ENV
 echo "workload_identity_service_account=${service_account}" >> $GITHUB_ENV
 echo "apigee_organisation=${apigee_organisation}" >> $GITHUB_ENV
-echo "target_url=http://7.0.8.5" >> $GITHUB_ENV
+echo "target_url=http://7.0.0.2" >> $GITHUB_ENV
