b6a1a19f8680413866bf81352c1597a1b3b10b4e
Ross Hockly <Ross.Hockly@PACONSULTING.COM>
2024-08-20 08:45:01 +0100

    Db null traid

--- Src/DfT.DTRO/Migrations/SeedData.cs
+++ Src/DfT.DTRO/Migrations/SeedData.cs
@@ -25,7 +25,7 @@ public static class SeedData
                             UserGroup = (int) UserGroup.Admin,
                             Name = "Department of Transport",
                             Prefix = "DfT",
-                            TraId = -1
+                            TraId = null
                         },
                         new DtroUser
                         {
