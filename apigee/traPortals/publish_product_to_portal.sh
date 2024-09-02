f947b801361f20b32ebefaca6d46e9347bcad99d
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-02 17:50:59 +0100

    DPPB-305: Update to publish open api spec - #12
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/traPortals/publish_product_to_portal.sh
+++ apigee/traPortals/publish_product_to_portal.sh
@@ -21,15 +21,29 @@ for PRODUCT in "${PRODUCT_NAMES[@]}"; do
 
   # Name of product created via product.json
   PRODUCT_NAME="${env_name_prefix}-${PRODUCT}"
-
+  echo "PRODUCT_NAME: {$PRODUCT_NAME}"
   # Convert product name to title case with spaces
   TITLE=$(to_title_case "${PRODUCT}")
+  echo "TITLE: {$TITLE}"
 
   # Construct the description
   DESCRIPTION="This is the D-TRO application for ${TITLE}s."
 
   # Make the API call
-  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites/${ORG}-${PORTAL_URL}/apidocs" \
+#  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites/${ORG}-${PORTAL_URL}/apidocs" \
+#    -H "Authorization: Bearer ${TOKEN}" \
+#    -H "Content-Type: application/json" \
+#    -d '{
+#      "title": "'"${TITLE}"'",
+#      "description": "'"${DESCRIPTION}"'",
+#      "anonAllowed": false,
+#      "imageUrl": "",
+#      "requireCallbackUrl": false,
+#      "categoryIds": [],
+#      "published": true,
+#      "apiProductName": "'"${PRODUCT_NAME}"'"
+#    }')
+  RESPONSE=$(curl -s  -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites/${ORG}-${PORTAL_URL}/apidocs" \
     -H "Authorization: Bearer ${TOKEN}" \
     -H "Content-Type: application/json" \
     -d '{
@@ -42,16 +56,17 @@ for PRODUCT in "${PRODUCT_NAMES[@]}"; do
       "published": true,
       "apiProductName": "'"${PRODUCT_NAME}"'"
     }')
+  echo "RESPONSE: {$RESPONSE}"
 
-  # Error checking and handling
-  if [ "$RESPONSE" -eq 200 ]; then
-    echo "${TITLE} successfully created in the Developer Portal."
-  elif [ "$RESPONSE" -eq 409 ]; then
-    echo "${TITLE} already exists in the Developer Portal."
-  else
-    echo "Failed to publish ${TITLE} to developer portal ${PORTAL_NAME}. HTTP response code: $RESPONSE"
-    exit 1
-  fi
+ # Error checking and handling
+#  if [ "$RESPONSE" -eq 200 ]; then
+#    echo "${TITLE} successfully created in the Developer Portal."
+#  elif [ "$RESPONSE" -eq 409 ]; then
+#    echo "${TITLE} already exists in the Developer Portal."
+#  else
+#    echo "Failed to publish ${TITLE} to developer portal ${PORTAL_NAME}. HTTP response code: $RESPONSE"
+#    exit 1
+#  fi
 
 done
 
