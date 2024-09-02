be7a2bd54bec5e020979917411bebb14d5b115cf
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-02 12:38:28 +0100

    Test apigee api call
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/traPortals/create_tra_portal.sh
+++ apigee/traPortals/create_tra_portal.sh
@@ -12,7 +12,27 @@ else
 fi
 
 # Loop through each element in the array
-for tra_element in "${tra_array[@]}"; do
+#for tra_element in "${tra_array[@]}"; do
+#
+#  # Make the API call
+#  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites" \
+#    -H "Authorization: Bearer ${TOKEN}" \
+#    -H "Content-Type: application/json" \
+#    -d '{
+#      "name": "'"${tra_element^} D-TRO Portal"'",
+#      "description": "'"This is the ${tra_element^} Portal for D-TRO"'"
+#    }')
+#
+#  # Error checking and handling
+#  if [ "$RESPONSE" -eq 200 ]; then
+#    echo "${tra_element^} Portal successfully created in ${ORG}."
+#  elif [ "$RESPONSE" -eq 409 ]; then
+#    echo "${tra_element^} Portal already exists in ${ORG}."
+#  else
+#    echo "Failed to create ${tra_element^} portal in ${ORG}. HTTP response code: $RESPONSE"
+#  fi
+#
+#done
 
   # Make the API call
   RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/sites" \
