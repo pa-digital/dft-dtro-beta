5034899f150ab966b9df012663b6d3e5ec48cac3
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-02 13:01:43 +0100

    Test apigee api call - #3
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/traPortals/create_tra_portal.sh
+++ apigee/traPortals/create_tra_portal.sh
@@ -35,15 +35,13 @@ fi
 #done
 
 # Make the API call to retrieve developer's details
-RESPONSE=$(curl -s -o /dev/null -X GET "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application" \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Content-Type: application/json")
-echo "$RESPONSE"
+RESPONSE=$(curl -s -X GET "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application" \
+  -H "Authorization: Bearer ${TOKEN}")
+echo "Response: $RESPONSE"
 consumerKey=$(echo "$RESPONSE" | jq -r '.credentials[0].consumerKey')
 apiproduct=$(echo "$RESPONSE" | jq -r '.credentials[0].apiProducts[0].apiproduct')
 
 # Make the API call approve api product
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
-  -H "Authorization: Bearer ${TOKEN}" \
-  -H "Content-Type: application/json")
+  -H "Authorization: Bearer ${TOKEN}")
 echo "$RESPONSE"
