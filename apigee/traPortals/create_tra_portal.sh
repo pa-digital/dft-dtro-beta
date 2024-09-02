0ff3f8db4cdaae7753f477c8d6d8ceb69bbbd743
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-02 12:56:52 +0100

    Test apigee api call - #2
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/traPortals/create_tra_portal.sh
+++ apigee/traPortals/create_tra_portal.sh
@@ -38,12 +38,12 @@ fi
 RESPONSE=$(curl -s -o /dev/null -X GET "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application" \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json")
-echo RESPONSE
-consumerKey=$(echo RESPONSE | jq -r '.credentials[0].consumerKey')
-apiproduct=$(echo RESPONSE | jq -r '.credentials[0].apiProducts[0].apiproduct')
+echo "$RESPONSE"
+consumerKey=$(echo "$RESPONSE" | jq -r '.credentials[0].consumerKey')
+apiproduct=$(echo "$RESPONSE" | jq -r '.credentials[0].apiProducts[0].apiproduct')
 
 # Make the API call approve api product
-RESPONSE=$(curl -s -o /dev/null -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/vighnesh.pindoria@dft.gov.uk/apps/ACME-Publisher-Application/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
   -H "Authorization: Bearer ${TOKEN}" \
   -H "Content-Type: application/json")
-echo RESPONSE
+echo "$RESPONSE"
