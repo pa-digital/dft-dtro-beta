8b9ce3c9a61a87c88d648b391f964587ecf55220
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-02 17:16:55 +0100

    DPPB-305: Update to publish open api spec - #6
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/traPortals/publish_product_to_portal.sh
+++ apigee/traPortals/publish_product_to_portal.sh
@@ -2,13 +2,8 @@
 
 # Script Variables
 ORG=$apigee_organisation
-YAML_FILE="../openApi/openapi3_0.yml"
-echo "files in path"
-for file in ../openApi/*; do
-    if [ -f "$file" ]; then
-        echo "$file"
-    fi
-done
+YAML_FILE="../openApi/openapi3_0.ymal"
+
 to_title_case() {
   echo "$1" | sed -e 's/\b./\u&/g' -e 's/-/ /g'
 }
