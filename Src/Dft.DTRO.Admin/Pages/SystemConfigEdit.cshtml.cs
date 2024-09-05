3725979ef7e23414fdbada8f3befd5c1d2dc583c
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2024-09-05 15:22:28 +0100

    adding logger on SystemConfig service

--- Src/Dft.DTRO.Admin/Pages/SystemConfigEdit.cshtml.cs
+++ Src/Dft.DTRO.Admin/Pages/SystemConfigEdit.cshtml.cs
@@ -3,10 +3,12 @@ using DfT.DTRO.Models.SystemConfig;
 public class SystemConfigEditModel : PageModel
 {
     private readonly ISystemConfigService _systemConfigService;
+    private readonly ILogger<SystemConfigEditModel> _logger;
 
-    public SystemConfigEditModel(ISystemConfigService systemConfigService)
+    public SystemConfigEditModel(ISystemConfigService systemConfigService, ILogger<SystemConfigEditModel> logger)
     {
         _systemConfigService = systemConfigService;
+        _logger = logger;
     }
 
     [BindProperty]
@@ -14,17 +16,21 @@ public class SystemConfigEditModel : PageModel
 
     public async Task OnGetAsync()
     {
+        _logger.LogInformation($"Method {nameof(OnGetAsync)} called at {DateTime.UtcNow:G}");
         SystemConfig = await _systemConfigService.GetSystemConfig();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
+        _logger.LogInformation($"Method {nameof(OnPostAsync)} called at {DateTime.UtcNow:G}");
         var action = Request.Form["action"];
         if (action == "Cancel")
         {
+            _logger.LogError($"Action {string.Join(" | ", action)} called at {DateTime.UtcNow:G}");
             return RedirectToPage("Index");
         }
-        await _systemConfigService.UpdateSystemConfig(SystemConfig);
+        var isUpdated = await _systemConfigService.UpdateSystemConfig(SystemConfig);
+        _logger.LogInformation($"Method 'UpdateSystemConfig' called at {DateTime.UtcNow:G} returned {isUpdated}");
         return RedirectToPage("Index");
     }
 }
