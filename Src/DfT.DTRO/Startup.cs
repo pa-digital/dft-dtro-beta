c041c03fa7293a5996638c64cd774c622efbcf60
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2024-09-06 14:07:48 +0100

    first deployment for integration

--- Src/DfT.DTRO/Startup.cs
+++ Src/DfT.DTRO/Startup.cs
@@ -99,7 +99,7 @@ public class Startup
 
         app.UseHealthChecks("/health");
 
-        //DbInitialize.EmptyDtroUsersTable(app);
+        DbInitialize.EmptyDtroUsersTable(app);
         DbInitialize.SeedAppData(app);
         if (env.IsDevelopment())
         {
