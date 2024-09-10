bf1b70c19788b51060ab3e9d4173978884a56473
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-10 14:58:02 +0100

    debug activate_user_app.sh
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/scripts/activate_user_app.sh
+++ apigee/scripts/activate_user_app.sh
@@ -13,11 +13,14 @@ consumerKey=$(echo "$RESPONSE" | jq -r '.credentials[0].consumerKey')
 apiproduct=$(echo "$RESPONSE" | jq -r '.credentials[0].apiProducts[0].apiproduct')
 
 # Make the API call to approve api product
-RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/${DEV_EMAIL}/apps/${APP_NAME}/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
+#RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/${DEV_EMAIL}/apps/${APP_NAME}/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
+#  -H "Authorization: Bearer ${TOKEN}")
+## Error checking and handling
+#  if [ "$RESPONSE" -eq 204 ]; then
+#    echo "${APP_NAME} Activated."
+#  else
+#    echo "Failed to activate ${APP_NAME}. HTTP response code: $RESPONSE"
+#  fi
+RESPONSE=$(curl -s -i -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/${DEV_EMAIL}/apps/${APP_NAME}/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
   -H "Authorization: Bearer ${TOKEN}")
-# Error checking and handling
-  if [ "$RESPONSE" -eq 204 ]; then
-    echo "${APP_NAME} Activated."
-  else
-    echo "Failed to activate ${APP_NAME}. HTTP response code: $RESPONSE"
-  fi
+echo "$RESPONSE"
