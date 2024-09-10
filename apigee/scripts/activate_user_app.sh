2a85548b721e778e3e9bdfce001103f0e10047f3
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-10 14:58:32 +0100

    debug activate_user_app.sh
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/scripts/activate_user_app.sh
+++ apigee/scripts/activate_user_app.sh
@@ -11,7 +11,10 @@ RESPONSE=$(curl -s -X GET "https://apigee.googleapis.com/v1/organizations/${ORG}
 # Extract key and product name
 consumerKey=$(echo "$RESPONSE" | jq -r '.credentials[0].consumerKey')
 apiproduct=$(echo "$RESPONSE" | jq -r '.credentials[0].apiProducts[0].apiproduct')
-
+echo "consumerKey"
+echo "$consumerKey"
+echo "apiproduct"
+echo "$apiproduct"
 # Make the API call to approve api product
 #RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/${DEV_EMAIL}/apps/${APP_NAME}/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
 #  -H "Authorization: Bearer ${TOKEN}")
@@ -23,4 +26,5 @@ apiproduct=$(echo "$RESPONSE" | jq -r '.credentials[0].apiProducts[0].apiproduct
 #  fi
 RESPONSE=$(curl -s -i -X POST "https://apigee.googleapis.com/v1/organizations/${ORG}/developers/${DEV_EMAIL}/apps/${APP_NAME}/keys/${consumerKey}/apiproducts/${apiproduct}?action=approve" \
   -H "Authorization: Bearer ${TOKEN}")
+echo "RESPONSE"
 echo "$RESPONSE"
