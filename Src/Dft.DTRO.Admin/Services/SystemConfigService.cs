620061030eef968495ac630d3ba4d3681d0ea86f
Ross Hockly <Ross.Hockly@PACONSULTING.COM>
2024-09-12 16:47:57 +0100

    Manual merge config service

--- Src/Dft.DTRO.Admin/Services/SystemConfigService.cs
+++ Src/Dft.DTRO.Admin/Services/SystemConfigService.cs
@@ -6,7 +6,8 @@ public class SystemConfigService : ISystemConfigService
     private readonly HttpClient _client;
     private readonly IXappIdService _xappIdService;
     private readonly IErrHandlingService _errHandlingService;
-    public SystemConfigService(IHttpClientFactory clientFactory, IXappIdService xappIdService, IErrHandlingService errHandlingService)
+    private readonly ILogger<ISystemConfigService> _logger;
+    public SystemConfigService(IHttpClientFactory clientFactory, IXappIdService xappIdService, ILogger<ISystemConfigService> logger, IErrHandlingService errHandlingService)
     {
         _client = clientFactory.CreateClient("ExternalApi");
         _xappIdService = xappIdService;
@@ -15,17 +16,27 @@ public class SystemConfigService : ISystemConfigService
 
     public async Task<bool> UpdateSystemConfig(SystemConfig systemConfig)
     {
+        _logger.LogInformation($"Method {nameof(UpdateSystemConfig)} called at {DateTime.UtcNow:G}");
         var content = JsonContent.Create(systemConfig);
         var request = new HttpRequestMessage(HttpMethod.Put, $"/systemConfig/updateFromBody/")
         {
             Content = content
         };
+        _logger.LogInformation($"Request headers: {request.Headers} generated at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Request uri: {request.RequestUri} generated at {DateTime.UtcNow:G}");
+        
         _xappIdService.AddXAppIdHeader(ref request);
+        _logger.LogInformation($"Client Base URL: {_client.BaseAddress} generated at {DateTime.UtcNow:G}");
 
         var response = await _client.SendAsync(request);
         await _errHandlingService.RedirectIfErrors(response);
 
+        _logger.LogInformation($"Response content: {response.Content} generated at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Response request message: {response.RequestMessage} generated at {DateTime.UtcNow:G}");
+
+
         _xappIdService.ChangeXAppId(systemConfig.xAppId);
+        _logger.LogInformation($"Client Base URL: {_client.BaseAddress} generated at {DateTime.UtcNow:G}");
         return true;
     }
     
@@ -38,9 +49,14 @@ public class SystemConfigService : ISystemConfigService
         {
             var request = new HttpRequestMessage(HttpMethod.Get, "/systemConfig");
             _xappIdService.AddXAppIdHeader(ref request);
+            _logger.LogInformation($"Client Base URL: {_client.BaseAddress} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Request headers: {request.Headers} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Request uri: {request.RequestUri} generated at {DateTime.UtcNow:G}");
 
             var response = await _client.SendAsync(request);
             await _errHandlingService.RedirectIfErrors(response);
+            _logger.LogInformation($"Response content: {response.Content} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Response request message: {response.RequestMessage} generated at {DateTime.UtcNow:G}");
 
             var jsonResponse = await response.Content.ReadAsStringAsync();
             if (jsonResponse == null)
@@ -50,9 +66,13 @@ public class SystemConfigService : ISystemConfigService
             var ret = JsonSerializer.Deserialize<SystemConfig>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             if (ret == null)
             {
+                _logger.LogError($"Response content {jsonResponse} generated at {DateTime.UtcNow:G}");
                 return unknown;
             }
+            _logger.LogInformation($"xAppId from response {ret.xAppId} generated at {DateTime.UtcNow:G}");
+
             ret.xAppId = _xappIdService.MyXAppId();
+            _logger.LogInformation($"xAppId from service {_xappIdService.MyXAppId()} generated at {DateTime.UtcNow:G}");
             return ret;
         }
         catch (Exception)
