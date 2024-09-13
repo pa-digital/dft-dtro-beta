098e55f7a1f125d539001a4d93bb3d1865194b32
Ross Hockly <Ross.Hockly@PACONSULTING.COM>
2024-09-13 08:32:35 +0100

    Copy main files

--- Src/Dft.DTRO.Admin/Pages/SystemConfigEdit.cshtml.cs
+++ Src/Dft.DTRO.Admin/Pages/SystemConfigEdit.cshtml.cs
@@ -3,12 +3,11 @@ using DfT.DTRO.Models.SystemConfig;
 public class SystemConfigEditModel : PageModel
 {
     private readonly ISystemConfigService _systemConfigService;
-    private readonly IErrHandlingService _errHandlingService;
     private readonly ILogger<SystemConfigEditModel> _logger;
-    public SystemConfigEditModel(ISystemConfigService systemConfigService, ILogger<SystemConfigEditModel> logger, IErrHandlingService errHandlingService)
+
+    public SystemConfigEditModel(ISystemConfigService systemConfigService, ILogger<SystemConfigEditModel> logger)
     {
         _systemConfigService = systemConfigService;
-        _errHandlingService = errHandlingService;
         _logger = logger;
     }
 
@@ -23,25 +22,19 @@ public class SystemConfigEditModel : PageModel
 
     public async Task<IActionResult> OnPostAsync()
     {
-        try
+        _logger.LogInformation($"Method {nameof(OnPostAsync)} called at {DateTime.UtcNow:G}");
+        var action = Request.Form["action"];
+        if (action == "Cancel")
         {
-            _logger.LogInformation($"Method {nameof(OnPostAsync)} called at {DateTime.UtcNow:G}");
-            var action = Request.Form["action"];
-            if (action == "Cancel")
-            {
-                return RedirectToPage("Index");
-            }
-            var isUpdated = await _systemConfigService.UpdateSystemConfig(SystemConfig);
-            _logger.LogInformation($"Method '{nameof(_systemConfigService.UpdateSystemConfig)}' called at {DateTime.UtcNow:G} returned {isUpdated}");
-            _logger.LogInformation($"x-App-Id\t'{SystemConfig.xAppId}' called at {DateTime.UtcNow:G}");
-            _logger.LogInformation($"Current User\t'{SystemConfig.CurrentUserName}' called at {DateTime.UtcNow:G}");
-            _logger.LogInformation($"Is in test?\t'{SystemConfig.IsTest}' called at {DateTime.UtcNow:G}");
-            _logger.LogInformation($"System name\t'{SystemConfig.SystemName}' called at {DateTime.UtcNow:G}");
+            _logger.LogError($"Action {string.Join(" | ", action)} called at {DateTime.UtcNow:G}");
             return RedirectToPage("Index");
         }
-        catch (Exception ex)
-        {
-            return _errHandlingService.HandleUiError(ex);
-        }
+        var isUpdated = await _systemConfigService.UpdateSystemConfig(SystemConfig);
+        _logger.LogInformation($"Method '{nameof(_systemConfigService.UpdateSystemConfig)}' called at {DateTime.UtcNow:G} returned {isUpdated}");
+        _logger.LogInformation($"x-App-Id\t'{SystemConfig.xAppId}' called at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Current User\t'{SystemConfig.CurrentUserName}' called at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Is in test?\t'{SystemConfig.IsTest}' called at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"System name\t'{SystemConfig.SystemName}' called at {DateTime.UtcNow:G}");
+        return RedirectToPage("Index");
     }
 }
--- Src/Dft.DTRO.Admin/Services/SystemConfigService.cs
+++ Src/Dft.DTRO.Admin/Services/SystemConfigService.cs
@@ -5,25 +5,34 @@ public class SystemConfigService : ISystemConfigService
 {
     private readonly HttpClient _client;
     private readonly IXappIdService _xappIdService;
-    private readonly IErrHandlingService _errHandlingService;
-    public SystemConfigService(IHttpClientFactory clientFactory, IXappIdService xappIdService, IErrHandlingService errHandlingService)
+    private readonly ILogger<ISystemConfigService> _logger;
+    public SystemConfigService(IHttpClientFactory clientFactory, IXappIdService xappIdService, ILogger<ISystemConfigService> logger)
     {
         _client = clientFactory.CreateClient("ExternalApi");
         _xappIdService = xappIdService;
-        _errHandlingService = errHandlingService;
+        _logger = logger;
     }
 
     public async Task<bool> UpdateSystemConfig(SystemConfig systemConfig)
     {
+        _logger.LogInformation($"Method {nameof(UpdateSystemConfig)} called at {DateTime.UtcNow:G}");
         var content = JsonContent.Create(systemConfig);
         var request = new HttpRequestMessage(HttpMethod.Put, $"/systemConfig/updateFromBody/")
         {
             Content = content
         };
+        _logger.LogInformation($"Request headers: {request.Headers} generated at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Request uri: {request.RequestUri} generated at {DateTime.UtcNow:G}");
+
         _xappIdService.AddXAppIdHeader(ref request);
+        _logger.LogInformation($"Client Base URL: {_client.BaseAddress} generated at {DateTime.UtcNow:G}");
 
         var response = await _client.SendAsync(request);
-        await _errHandlingService.RedirectIfErrors(response);
+        _logger.LogInformation($"Response content: {response.Content} generated at {DateTime.UtcNow:G}");
+        _logger.LogInformation($"Response request message: {response.RequestMessage} generated at {DateTime.UtcNow:G}");
+
+
+        response.EnsureSuccessStatusCode();
 
         _xappIdService.ChangeXAppId(systemConfig.xAppId);
         return true;
@@ -32,26 +41,38 @@ public class SystemConfigService : ISystemConfigService
 
     public async Task<SystemConfig> GetSystemConfig()
     {
+        _logger.LogInformation($"Method {nameof(GetSystemConfig)} called at {DateTime.UtcNow:G}");
+
         var unknown = new SystemConfig() { SystemName = "Unknown", IsTest = false };
         try
         {
             var request = new HttpRequestMessage(HttpMethod.Get, "/systemConfig");
             _xappIdService.AddXAppIdHeader(ref request);
+            _logger.LogInformation($"Client Base URL: {_client.BaseAddress} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Request headers: {request.Headers} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Request uri: {request.RequestUri} generated at {DateTime.UtcNow:G}");
 
             var response = await _client.SendAsync(request);
-            await _errHandlingService.RedirectIfErrors(response);
+            _logger.LogInformation($"Response content: {response.Content} generated at {DateTime.UtcNow:G}");
+            _logger.LogInformation($"Response request message: {response.RequestMessage} generated at {DateTime.UtcNow:G}");
 
+            response.EnsureSuccessStatusCode();
             var jsonResponse = await response.Content.ReadAsStringAsync();
             if (jsonResponse == null)
             {
+                _logger.LogError($"Response content {jsonResponse} generated at {DateTime.UtcNow:G}");
                 return unknown;
             }
             var ret = JsonSerializer.Deserialize<SystemConfig>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             if (ret == null)
             {
+                _logger.LogError($"System configuration {ret} generated at {DateTime.UtcNow:G}");
                 return unknown;
             }
+            _logger.LogInformation($"xAppId from response {ret.xAppId} generated at {DateTime.UtcNow:G}");
+
             ret.xAppId = _xappIdService.MyXAppId();
+            _logger.LogInformation($"xAppId from service {_xappIdService.MyXAppId()} generated at {DateTime.UtcNow:G}");
             return ret;
         }
         catch (Exception)
