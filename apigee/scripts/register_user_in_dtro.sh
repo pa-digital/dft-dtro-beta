f2fe6a081d9607f11e5b2b98dfcc8668c275652c
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-19 12:32:08 +0100

    DPPB-501: initail commit
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/scripts/register_user_in_dtro.sh
+++ apigee/scripts/register_user_in_dtro.sh
@@ -2,52 +2,51 @@
 
 # Script Variables
 ORG=$apigee_organisation
-CLIENT_ID=$CLIENT_ID
-CLIENT_SECRET=$CLIENT_SECRET
 uuid=$(uuidgen)
+TRA_ID=$SWA_CODE
 
+# Get OAuth access token
 OAUTH_RESPONSE=$(curl -X POST "https://dtro-integration.dft.gov.uk/v1/oauth-generator" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -u ${CLIENT_ID}:${CLIENT_SECRET} \
   -d "grant_type=client_credentials")
 
-echo "OAUTH_RESPONSE"
-echo "${OAUTH_RESPONSE}"
-
 # Extract access token and appId
 access_token=$(echo "$OAUTH_RESPONSE" | jq -r '.access_token')
 app_id=$(echo "$OAUTH_RESPONSE" | jq -r '.application_name')
 
-RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X GET 'https://dtro-integration.dft.gov.uk/v1/healthApi' \
-  -H "Authorization: Bearer ${access_token}" \
-  -H "X-Correlation-ID: ${app_id}"
-)
-
-# Error checking and handling
- if [ "$RESPONSE" -eq 200 ]; then
-   echo "API is healthy"
- else
-   echo "API is unhealthy. HTTP response code: $RESPONSE"
- fi
-
-#RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST 'https://dtro-test.dft.gov.uk/v1/dtroUsers/createFromBody' \
-#  -H 'X-Correlation-ID: 41ae0471-d7de-4737-907f-cab2f0089796' \
-#  -H 'Content-Type: application/json' \
-#  -H 'Accept: text/plain' \
-#  -H "Authorization: Bearer ${TOKEN}" \
-#  -d '{
-#    "id": "'${uuid}'",
-#    "traId": "<integer>",
-#    "name": "<string>",
-#    "prefix": "<string>",
-#    "userGroup": "admin",
-#    "xAppId": "<uuid>"
-#  }')
+## Check Health of D-TRO Platform
+#HEALTH_API_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X GET 'https://dtro-integration.dft.gov.uk/v1/healthApi' \
+#  -H "Authorization: Bearer ${access_token}" \
+#  -H "X-Correlation-ID: ${app_id}"
+#)
 #
 ## Error checking and handling
-#  if [ "$RESPONSE" -eq 204 ]; then
-#    echo "${APP_NAME} Activated."
-#  else
-#    echo "Failed to activate ${APP_NAME}. HTTP response code: $RESPONSE"
-#  fi
+#if [ "$HEALTH_API_RESPONSE" -eq 200 ]; then
+# echo "API is healthy"
+#else
+# echo "API is unhealthy. HTTP response code: $HEALTH_API_RESPONSE"
+# exit 1
+#fi
 
+# Add user to D-TRO
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X POST 'https://dtro-integration.dft.gov.uk/v1/dtroUsers/createFromBody' \
+  -H 'X-Correlation-ID: 41ae0471-d7de-4737-907f-cab2f0089796' \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: text/plain' \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -d '{
+    "id": "'${uuid}'",
+    "xAppId": "'${app_id}'",
+    "name": "'${APP_NAME}'",
+    "prefix": "'${APP_PREFIX}'",
+    "userGroup": "'${USER_GROUP}'"
+  }')
+
+# Error checking and handling
+  if [ "$RESPONSE" -eq 200 ]; then
+    echo "${APP_NAME}(${app_id}) added to D-TR0"
+  else
+    echo "Failed to add ${APP_NAME}(${app_id}) to D-TR0. HTTP response code: $RESPONSE"
+    exit 1
+  fi
