7ddbdfb35930204e7eb44485cd7f29e19aa77aaa
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-09-25 11:24:09 +0100

    DPPB-305: register_user_in_dtro.sh debug
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/scripts/register_user_in_dtro.sh
+++ apigee/scripts/register_user_in_dtro.sh
@@ -32,16 +32,17 @@ echo "Got access token"
 if [ "$IS_PUBLISHER" = true ]; then
   # Add Publisher user (tra) to D-TRO
   APP_ID_CLEAN=$(echo "$APP_ID" | tr -d '"')
-  echo "APP_ID"
-  echo '"'"${APP_ID}"'"'
-  RESPONSE=$(curl -w "%{http_code}" -s -X POST 'https://dtro-integration.dft.gov.uk/v1/dtroUsers/createFromBody' \
+  echo "APP_ID_CLEAN"
+  echo "$APP_ID_CLEAN"
+  echo ''"$APP_ID_CLEAN"''
+  RESPONSE=$(curl -X POST 'https://dtro-integration.dft.gov.uk/v1/dtroUsers/createFromBody' \
     -H 'X-Correlation-ID: 41ae0471-d7de-4737-907f-cab2f0089796' \
     -H 'Content-Type: application/json' \
     -H 'Accept: text/plain' \
     -H "Authorization: Bearer ${access_token}" \
     -d '{
       "id": "'"${uuid}"'",
-      "xAppId": "'"${APP_ID}"'",
+      "xAppId": '"$APP_ID_CLEAN"',
       "traId": "'${TRA_ID}'",
       "name": "'"${APP_NAME}"'",
       "prefix": "'"${APP_PREFIX}"'",
@@ -51,10 +52,6 @@ if [ "$IS_PUBLISHER" = true ]; then
   echo "RESPONSE-tra"
   echo "${RESPONSE}"
 
-
-  # Output the HTTP status code and the response body
-  echo "HTTP Status Code: $HTTP_CODE"
-  echo "Response Body: $BODY"
 #  response_json=$(echo "$RESPONSE" | jq )
 #  echo "${response_json}"
   # Error checking and handling
