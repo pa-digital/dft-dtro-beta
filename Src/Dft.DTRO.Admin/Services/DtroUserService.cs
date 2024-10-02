651f6d1266d75cabe8be664e385c129210ffe589
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-10-02 17:05:44 +0100

    DPPB-634: Update to sent array rather than list if ids
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- Src/Dft.DTRO.Admin/Services/DtroUserService.cs
+++ Src/Dft.DTRO.Admin/Services/DtroUserService.cs
@@ -116,7 +116,9 @@ public class DtroUserService : IDtroUserService
     public async Task<bool> DeleteDtroUserAsync(List<Guid> dtroUserIds)
     {
 //        var ids = string.Join(",", dtroUserIds);
-        var content = JsonContent.Create(new { Ids = dtroUserIds });
+//        var content = JsonContent.Create(new { Ids = dtroUserIds });
+        var content = new StringContent(JsonSerializer.Serialize(new { Ids = dtroUserIds }), Encoding.UTF8, "application/json");
+
         var request =
             new HttpRequestMessage(HttpMethod.Delete, ConfigHelper.Version + $"/dtroUsers/redundant")
             {
