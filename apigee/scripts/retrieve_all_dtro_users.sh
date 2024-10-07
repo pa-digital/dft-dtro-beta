0beb2bdedda41451d85e9c37ab7ee8e3b94fcfae
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-10-07 12:26:35 +0100

    Github workflow for CSOs to retrieve all dtro users
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

---
+++ apigee/scripts/retrieve_all_dtro_users.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get OAuth access token
+OAUTH_RESPONSE=$(curl -X POST "https://dtro-integration.dft.gov.uk/v1/oauth-generator" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -u ${CLIENT_ID}:${CLIENT_SECRET} \
+  -d "grant_type=client_credentials")
+
+# Extract access token and appId
+access_token=$(echo "$OAUTH_RESPONSE" | jq -r '.access_token')
+echo " "
+echo "Access token retrieved"
+echo " "
+
+## Check Health of D-TRO Platform
+RESPONSE=$(curl-X GET 'https://dtro-integration.dft.gov.uk/v1/dtroUsers' \
+  -H "Authorization: Bearer ${access_token}" \
+  -H "X-Correlation-ID: 41ae0471-d7de-4737-907f-cab2f0089796"
+)
+echo " "
+echo "Response for Retrieve All Dtro Users:"
+echo " "
+echo "$RESPONSE"
