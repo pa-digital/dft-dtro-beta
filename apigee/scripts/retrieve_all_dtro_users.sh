b801eec130c2695f7a8a638d73cf1d8e406fe130
VinP-PA <vighneshgpindoria@yahoo.co.uk>
2024-10-07 13:09:12 +0100

    Correct curl command
    Signed-off-by: VinP-PA <vighneshgpindoria@yahoo.co.uk>

--- apigee/scripts/retrieve_all_dtro_users.sh
+++ apigee/scripts/retrieve_all_dtro_users.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
 DOMAIN=$domain
+echo "$DOMAIN"
 
 # Get OAuth access token
-OAUTH_RESPONSE=$(curl -X POST "https://${DOMAIN}.dft.gov.uk/v1/oauth-generator" \
+OAUTH_RESPONSE=$(curl -s -X POST "https://${DOMAIN}.dft.gov.uk/v1/oauth-generator" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -u ${CLIENT_ID}:${CLIENT_SECRET} \
   -d "grant_type=client_credentials")
 
 # Extract access token and appId
-access_token=$(echo "$OAUTH_RESPONSE" | jq -r '.access_token')
-echo " "
-echo "Access token retrieved"
-echo " "
+if [ -z "$OAUTH_RESPONSE" ] || [ "$OAUTH_RESPONSE" == "[]" ]; then
+  echo "Response is empty"
+  exit 1
+else
+  access_token=$(echo "$OAUTH_RESPONSE" | jq -r '.access_token')
+  echo " "
+  echo "Access token retrieved"
+  echo "$OAUTH_RESPONSE" | jq .
+  echo " "
+fi
 
 ## Get
-RESPONSE=$(curl -X GET "https://${DOMAIN}.dft.gov.uk/v1/dtroUsers" \
+RESPONSE=$(curl -s -X GET "https://${DOMAIN}.dft.gov.uk/v1/dtroUsers" \
   -H 'X-Correlation-ID: 41ae0471-d7de-4737-907f-cab2f0089796' \
   -H 'Accept: text/plain' \
   -H "Authorization: Bearer ${access_token}"
 )
-number_of_users=$(echo "$RESPONSE" | jq '. | length')
-echo " "
-echo "Number of users found: $number_of_users"
-echo " "
-echo "$RESPONSE" | jq .
+if [ -z "$RESPONSE" ] || [ "$RESPONSE" == "[]" ]; then
+  echo "Response is empty"
+  exit 1
+else
+  number_of_users=$(echo "$RESPONSE" | jq '. | length')
+  echo " "
+  echo "Number of users found: $number_of_users"
+  echo " "
+  echo "$RESPONSE" | jq .
+fi
