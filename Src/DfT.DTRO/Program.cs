49d87d2e83d7a0eca70bd9b108a6f3bba7f99f12
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2024-10-08 15:22:01 +0100

    Add google auth to the code before diagnostic

--- Src/DfT.DTRO/Program.cs
+++ Src/DfT.DTRO/Program.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
 using Google.Cloud.Diagnostics.Common;
 using Microsoft.AspNetCore.Connections;
 
@@ -14,12 +17,22 @@ public class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureServices(services =>
             {
+                using (var stream = new FileStream("", FileMode.Open, FileAccess.Read))
+                {
+                    var userCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.FromStream(stream).Secrets,
+                        new[] { "dtro-dev-dft-dtro-beta" },
+                        "gabriel.popescu@dft.gov.uk",
+                        CancellationToken.None,
+                        new FileDataStore("dft-dtro-dev-01")).Result;
+                }
+
                 services
                     .AddGoogleDiagnostics("dft-dtro-dev-01",
-                        "dtro-dev-dft-dtro-beta",
-                        traceOptions: TraceOptions.Create(),
-                        loggingOptions: LoggingOptions.Create(),
-                        errorReportingOptions: ErrorReportingOptions.Create());
+                            "dtro-dev-dft-dtro-beta",
+                            traceOptions: TraceOptions.Create(),
+                            loggingOptions: LoggingOptions.Create(),
+                            errorReportingOptions: ErrorReportingOptions.Create());
             })
             .ConfigureAppConfiguration((context, config) =>
             {
