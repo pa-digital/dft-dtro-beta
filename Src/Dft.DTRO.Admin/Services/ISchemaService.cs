bf93af8e74cdbdb15805d8ab5ee5a99e43888320
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2024-10-21 15:32:30 +0100

    schema version can be deleted once deactivated

--- Src/Dft.DTRO.Admin/Services/ISchemaService.cs
+++ Src/Dft.DTRO.Admin/Services/ISchemaService.cs
@@ -3,8 +3,14 @@ namespace Dft.DTRO.Admin.Services;
 public interface ISchemaService
 {
     Task ActivateSchemaAsync(string version);
+
     Task CreateSchemaAsync(string version, IFormFile file);
+
     Task DeactivateSchemaAsync(string version);
+
     Task<List<SchemaTemplateOverview>> GetSchemaVersionsAsync();
+
     Task UpdateSchemaAsync(string version, IFormFile file);
+
+    Task DeleteSchemaAsync(string version);
 }
