dcbfed7e8c5163563236caca292664a6ddf41e46
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2025-02-03 15:12:34 +0000

    adding tests files for integration tests

--- Src/DfT.DTRO.IntegrationTests/GlobalUsings.cs
+++ Src/DfT.DTRO.IntegrationTests/GlobalUsings.cs
@@ -1,7 +1,10 @@
 // Global using directives
 
+global using System;
+global using System.IO;
 global using System.Net;
 global using System.Net.Http;
+global using System.Text;
 global using System.Threading.Tasks;
 global using DfT.DTRO.DAL;
 global using Microsoft.AspNetCore.Mvc.Testing;
