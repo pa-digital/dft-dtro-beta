aa738054af50fbceb712f87952312dbdfb9ee0d5
Cameron Auld <cameron.auld@paconsulting.com>
2025-02-07 09:10:19 +0000

    Used resharper to refactor code to retrieve file path

--- Src/DfT.DTRO/Utilities/OSHelper.cs
+++ Src/DfT.DTRO/Utilities/OSHelper.cs
@@ -6,23 +6,24 @@ public static class OSHelper
 {
     public static string GetOSAppDataPath()
     {
-        string filePath = string.Empty;
+        var filePath = string.Empty;
         var dtroDataFolder = "DtroAppData";
         var dataExportFolder = "ExportedData";
         
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            var homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            filePath = Path.Combine(homeDirectory, "AppData", dtroDataFolder, dataExportFolder);
+            filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), 
+                "AppData", dtroDataFolder, dataExportFolder);
         }
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
-            var homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            filePath = Path.Combine(homeDirectory, "Library", "Application Support",  dtroDataFolder, dataExportFolder);
+            filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), 
+                "Library", "Application Support",  dtroDataFolder, dataExportFolder);
         }
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
-            filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), dtroDataFolder, dataExportFolder);
+            filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+                dtroDataFolder, dataExportFolder);
         }
         
         return filePath;
