379e818e4fc32f45de06eabd1e433aeb9c4533d7
Marcus Cumming <23223411+MarcusQA@users.noreply.github.com>
2025-02-20 16:54:55 +0000

    Extend to other files

--- Src/DfT.DTRO.IntegrationTests/IntegrationTests/SendDtroIntegrationTests.cs
+++ Src/DfT.DTRO.IntegrationTests/IntegrationTests/SendDtroIntegrationTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DfT.DTRO.IntegrationTests.IntegrationTests.Helpers;
 using DfT.DTRO.IntegrationTests.IntegrationTests.Helpers.DataEntities;
 
@@ -5,13 +6,25 @@ namespace DfT.DTRO.IntegrationTests.IntegrationTests
 {
     public class SendDtroIntegrationTests : BaseTest
     {
-        [Fact]
-        public async Task CreateDtroShouldBeSavedCorrectly()
+        public static IEnumerable<object[]> GetFileNames()
+        {
+            var directoryPath = new DirectoryInfo(TestConfig.AbsolutePathToDtroExamplesFolder);
+            FileInfo[] files = directoryPath.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                yield return new object[] { file.Name };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetFileNames))]
+        public async Task CreateDtroShouldBeSavedCorrectly(string filePath)
         {
             var userResponse = await DtroUsers.CreateUserAsync(User.Publisher);
             Assert.Equal(HttpStatusCode.Created, userResponse.StatusCode);
 
-            var dtroResponse = await Dtros.CreateDtroAsync(User.Publisher, $"JSON-{TestConfig.SchemaVersionUnderTest}-example-Derbyshire 2024 DJ388 partial.json");
+            var dtroResponse = await Dtros.CreateDtroAsync(User.Publisher, filePath);
             Assert.Equal(HttpStatusCode.Created, dtroResponse.StatusCode);
         }
     }
