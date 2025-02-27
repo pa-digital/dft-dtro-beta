c814a22dcd6b30dfac01e72d2e921c73fbc0dac6
Marcus Cumming <23223411+MarcusQA@users.noreply.github.com>
2025-02-27 16:50:11 +0000

    wip

--- run_unit_tests_with_coverage.sh
+++ run_unit_tests_with_coverage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
+LINE_THRESHOLD="21.37"
+PROJECT_NAME="Dft.DTRO.Tests"
  
 dotnet tool install -g dotnet-reportgenerator-globaltool
   
-dotnet test Src/Dft.DTRO.Tests/Dft.DTRO.Tests.csproj \
+dotnet test Src/$PROJECT_NAME/$PROJECT_NAME.csproj --verbosity normal \
 /p:CollectCoverage=true \
 /p:Include="[DfT.DTRO]*" \
-/p:Threshold=21.37 /p:ThresholdType=line \
-/p:Threshold=31.63 /p:ThresholdType=branch \
-/p:Threshold=35.02 /p:ThresholdType=method \
+/p:Threshold=$LINE_THRESHOLD /p:ThresholdType=line \
 /p:CoverletOutput=Coverage/coverage.json \
 /p:CoverletOutputFormat=opencover && \
-reportgenerator -reports:Src/Dft.DTRO.Tests/Coverage/coverage.json -targetdir:TestReports/UnitTests -reporttypes:Html
+reportgenerator -reports:Src/$PROJECT_NAME/Coverage/coverage.json -targetdir:TestReports/UnitTests -reporttypes:Html
