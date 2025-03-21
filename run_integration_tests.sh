c3c16dad4aa622c6cde01fdf6cea8123a206edd2
Marcus Cumming <199806864+MarcusCummingPA@users.noreply.github.com>
2025-03-21 14:04:06 +0000

    4 failing tests on comingIntoForceDate

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -2,4 +2,5 @@ ENVIRONMENT=${1:-local}
 echo "Environment: $ENVIRONMENT"
 
 cd Src/DfT.DTRO.IntegrationTests
+dotnet build
 ENVIRONMENT="$ENVIRONMENT" dotnet test --logger "trx;LogFileName=test-results.trx"
