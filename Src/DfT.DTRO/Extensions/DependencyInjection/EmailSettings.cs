8fd114b153699eed6e51ef917401e01fc92348ae
Gabriel-Popescu <gabriel.popescu@paconsulting.com>
2025-04-08 08:46:00 +0100

    remove sensitive data from the repository

--- Src/DfT.DTRO/Extensions/DependencyInjection/EmailSettings.cs
+++ Src/DfT.DTRO/Extensions/DependencyInjection/EmailSettings.cs
@@ -10,11 +10,25 @@ public static class EmailSettings
     /// </summary>
     public static string ApiKey { get; set; }
 
+    /// <summary>
+    /// Gets or sets the sender for the email service.
+    /// </summary>
+    public static string EmailSender { get; set; }
+
+    /// <summary>
+    /// Gets or sets the CSO contact for the email service.
+    /// </summary>
+    public static string CsoEmail { get; set; }
+
    
     /// <summary>
     /// Adds email settings to the service collection.
     /// </summary>
     /// <param name="services">The service collection to add the settings to.</param>
-    public static void AddEmailSettings(this IServiceCollection services) => 
+    public static void AddEmailSettings(this IServiceCollection services)
+    {
         ApiKey = Environment.GetEnvironmentVariable("EMAIL_API_KEY");
+        EmailSender = Environment.GetEnvironmentVariable("EMAIL_SENDER");
+        CsoEmail = Environment.GetEnvironmentVariable("CSO_EMAIL");
+    }
 }
--- Src/DfT.DTRO/Services/EmailService.cs
+++ Src/DfT.DTRO/Services/EmailService.cs
@@ -9,12 +9,12 @@ public class EmailService : IEmailService
         try
         {
             var client = new SendGridClient(EmailSettings.ApiKey);
-            var from = new EmailAddress("ben.payley@dft.gov.uk", "DfT Central Service Operator");
+            var from = new EmailAddress(EmailSettings.EmailSender, "DfT Central Service Operator");
             var to = new EmailAddress(requestEmail, "Application created");
             var subject = $"Approval for {app.Name}";
             var body = $"Your request for '{app.Name}' app has been approved. " +
                        "Please log in to the D-TRO portal to view your app credentials. " +
-                       "If you need assistance please email 'dtro-cso@dft.gov.uk'. " +
+                       $"If you need assistance please email '{EmailSettings.CsoEmail}'. " +
                        "Do not reply to this email.";
             var email = MailHelper.CreateSingleEmail(from, to, subject, body, string.Empty);
             await client.SendEmailAsync(email, CancellationToken.None);
