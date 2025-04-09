de9692aa9e47925eddb6dbd36dbea64ae4cacc03
Marcus Cumming <199806864+MarcusCummingPA@users.noreply.github.com>
2025-04-09 11:45:06 +0100

    Impose threshold on unit-test coverage

--- run_unit_tests_with_coverage.sh
+++ run_unit_tests_with_coverage.sh
@@ -12,4 +12,9 @@ dotnet test Src/$PROJECT_NAME/$PROJECT_NAME.csproj --verbosity normal \
 /p:CoverletOutput=Coverage/coverage.json \
 /p:CoverletOutputFormat=opencover
 
+if [ $? -ne 0 ]; then
+  echo "Tests failed or coverage threshold not met."
+  exit 1
+fi
+
 reportgenerator -reports:Src/$PROJECT_NAME/Coverage/coverage.json -targetdir:TestReports/UnitTests -reporttypes:Html
